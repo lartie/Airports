94df7654fa381f5e3df571249834db8991eb6861
Artie <log.wil.log@gmail.com>
2016-07-28 13:19:26 +0500

    -change connection for models and migrations

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -14,11 +14,6 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     /**
-     * @var string
-     */
-    protected $connection = 'flyinghigh';
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array
